5f473bcdc801bfc13a088aeefb788f81acdbdadf
okuhara <okutaku0507@gmial.com>
2013-04-18 18:28:22 +0900

    Rails初期ソースツリー

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sumire::Application.config.secret_token = '1580c9b40d906691d970e3c5874198f78816e2a6421f478db8b6ac76eb1d5a642cbbd0535bde3293a5400c1da026c0ca01e748f565d46b93612af96e4c6a33b7'
